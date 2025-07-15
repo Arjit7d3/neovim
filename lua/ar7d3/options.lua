374a688ffebdd64d5959831e46b6384ad1e044e3
Arjit Rajvanshi <arjitsystem03@gmail.com>
2025-07-15 19:24:54 +0530

    options

---
+++ lua/ar7d3/options.lua
@@ -0,0 +1,21 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+
+vim.opt.wrap = false
+
+vim.opt.smartindent = true
+vim.opt.smarttab = true
+vim.opt.autoindent = true
+
+vim.opt.breakindent = true
+
+vim.opt.undofile = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
