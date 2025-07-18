b93918cd3ec53801e9b87f1349d2fe4607850915
Arjit Rajvanshi <arjitsystem03@gmail.com>
2025-07-18 15:42:52 +0530

    Autoinstall=true

--- lua/ar7d3/plugins.lua
+++ lua/ar7d3/plugins.lua
@@ -23,6 +23,7 @@ return {
 
             configs.setup({
                 ensure_installed = { "c", "lua", "vim", "javascript", "python" },
+                auto_install = true,
                 sync_install = false,
                 highlight = { enable = true },
                 indent = { enable = true },
